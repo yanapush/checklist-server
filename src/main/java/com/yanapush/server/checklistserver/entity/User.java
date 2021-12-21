68e53787e86ab8f3c81198e121f2177d23bf09af
yanapush <pushkarevyy@mail.ru>
2021-12-21 18:05:06 +0300

    bugs with user fixed

--- src/main/java/com/yanapush/server/checklistserver/entity/User.java
+++ src/main/java/com/yanapush/server/checklistserver/entity/User.java
@@ -8,7 +8,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String last_update = "";
+    private String last_update = "empty";
     @Column(unique = true)
     private int password;
     @ManyToOne
